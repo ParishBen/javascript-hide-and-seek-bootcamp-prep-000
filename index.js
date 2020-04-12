63e29663fb34f2934d1233847edf2d6dd9e70561
Ben Parish <parishben13@gmail.com>
2020-04-12 00:02:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+function getFirstSelector(s) {
+  return document.querySelector(s);
+}
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+function deepestChild(){
+  
+  let grandNode = document.getElementById('grand-node');
+  let childOne = grandNode.children[0];
+ 
+  while (childOne) {
+    grandNode = childOne;
+    childOne = grandNode.children[0];
+  }
+ 
+  return grandNode;
 
+}
+
+
+function increaseRankBy(n) {
+  const allRankedLists = document.querySelectorAll('.ranked-list');
+ 
+  for (let i = 0, l = allRankedLists.length; i < l; i++) {
+    let allChildren = allRankedLists[i].children;
+ 
+    for (let r = 0, len = allChildren.length; r < len; r++) {
+      allChildren[r].innerHTML = parseInt(allChildren[r].innerHTML) + n;
+    }
+  }
+}
